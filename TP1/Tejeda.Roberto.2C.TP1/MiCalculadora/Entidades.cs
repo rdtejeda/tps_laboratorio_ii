e3c1bd5e2a9f4a6aba21296125862f3b857eec49
rdtejeda <roberto.daniel.tejeda@gmail.com>
2022-04-15 10:12:42 -0300

    15 abril 10:12

---
+++ TP1/Tejeda.Roberto.2C.TP1/MiCalculadora/Entidades.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiCalculadora
+{
+    static class Calculadora
+    {
+    }
+    public class Operando
+    {
+    }
+}
---
+++ TP1/Tejeda.Roberto.2C.TP1/MiCalculadora/FormCalculadora.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace MiCalculadora
+{
+    partial class FormCalculadora
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FormCalculadora";
+            this.Text = "Calculadora de Roberto Tejeda del curso 2ºC";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
