60ea59f54d7503263c4e8d08b28ac23c0230eb2c
rdtejeda <roberto.daniel.tejeda@gmail.com>
2022-06-18 15:35:57 +0000

    18jun1535

---
+++ TP4/Tejeda.Roberto.2C.TPFinal/Entidades/ClienteDTV.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersistirDatos;
+using Excepciones;
+using static System.Environment;
+
+namespace Entidades
+{
+    public class ClienteDTV : Persona
+    {
+        private string direccion;
+        private Servicio servicio;
+        public ClienteDTV()
+        {
+        }
+        public ClienteDTV(string dni, string nombre, string apellido, string direccion) : base(dni, nombre, apellido)
+        {
+           this.direccion = direccion;
+        }
+        public ClienteDTV(string dni, string nombre, string apellido, string direccion, Servicio servicio) : this(dni, nombre, apellido, direccion)
+        {
+            this.servicio = servicio;
+        }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public Servicio Servicio { get => servicio; set => servicio = value; }
+        public override string ToString()
+        {
+            return $"{Nombre} {Apellido} - {Direccion} - {Dni}";
+        }
+        /// <summary>
+        /// Identifica si un Cliente (DNI) esta en la base de datos 
+        /// </summary>
+        /// <param name="dni"></param>
+        /// <returns>Retorna el cliente</returns>
+        public static ClienteDTV IdentificarCliente(string dni)
+        {
+            ClienteDTV retorno = null;
+            string path = null;
+            try
+            {
+                if (((path = GetFolderPath(SpecialFolder.Desktop) + @"\TP4\") is not null))
+                {
+                    Serializar<List<ClienteDTV>> listaSerializadaC = new Serializar<List<ClienteDTV>>();
+                    List<ClienteDTV> listaClientesosDTV = listaSerializadaC.Leer(path, "clientesDTV.xml");
+                    foreach (ClienteDTV item in listaClientesosDTV)
+                    {
+                        if (item.Dni == dni)
+                        {
+                            retorno = item;
+                            return retorno;
+                        }
+                    }
+                    throw new ClienteNoDisponibleException($"el Dni {dni}.");
+                }
+            }
+            catch (ClienteNoDisponibleException)
+            {
+                throw;
+            }
+            catch (ArchivoException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return retorno;
+        }
+        /// <summary>
+        /// Agrega un nuevo cliente a la base de datos
+        /// </summary>
+        /// <param name="cliente"></param>
+        public static void AgregarNuevoCliente(ClienteDTV cliente)
+        {
+            string path = null;
+            try
+            {
+                if (((path = GetFolderPath(SpecialFolder.Desktop) + @"\TP4\") is not null))
+                {
+                    Serializar<List<ClienteDTV>> listaSerializadaC = new Serializar<List<ClienteDTV>>();                   
+                    List<ClienteDTV> listaClientesosDTV = listaSerializadaC.Leer(path, "clientesDTV.xml");
+                    listaClientesosDTV.Add(cliente);
+                    listaSerializadaC.Guardar(path, "clientesDTV.xml", listaClientesosDTV); 
+                }
+            }
+            catch (ClienteNoDisponibleException)
+            {
+                throw;
+            }
+            catch (ArchivoException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        /// <summary>
+        /// Modifica los servicios de un cliente
+        /// </summary>
+        /// <param name="cliente"></param>
+        public static void ModificarServiviosCliente(ClienteDTV cliente)
+        {
+            string path = null;
+            try
+            {
+                if (((path = GetFolderPath(SpecialFolder.Desktop) + @"\TP4\") is not null))
+                {
+                    Serializar<List<ClienteDTV>> listaSerializadaC = new Serializar<List<ClienteDTV>>();
+                    List<ClienteDTV> listaClientesosDTV = listaSerializadaC.Leer(path, "clientesDTV.xml");
+                    foreach (ClienteDTV item in listaClientesosDTV)
+                    {
+                        if(item.Dni ==cliente.Dni)
+                        {
+                            listaClientesosDTV.Remove(item);
+                            listaClientesosDTV.Add(cliente);
+                            break;
+                        }
+                    }                    
+                    listaSerializadaC.Guardar(path, "clientesDTV.xml", listaClientesosDTV);
+                }
+            }
+            catch (ClienteNoDisponibleException)
+            {
+                throw;
+            }
+            catch (ArchivoException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ TP4/Tejeda.Roberto.2C.TPFinal/Entidades/UsuarioDTV.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excepciones;
+using PersistirDatos;
+using static System.Environment;
+
+
+namespace Entidades
+{
+    public class UsuarioDTV : Persona
+    {
+        private string nombreUsuario;
+        private string password;
+        public UsuarioDTV()
+        {
+        }
+        public UsuarioDTV(string dni, string nombre, string apellido) : base(dni, nombre, apellido)
+        {
+        }
+        public UsuarioDTV(string dni, string nombre, string apellido, string nombreUsuario, string password)
+            : base(dni, nombre, apellido)
+        {
+            this.nombreUsuario = nombreUsuario;
+            this.password = password;
+        }
+        public string NombreUsuario { get => nombreUsuario; set => nombreUsuario = value; }
+        public string Passwword { get => password; set => password = value; }
+        public override string ToString()
+        {
+            return $"{Nombre} - {Apellido} - {NombreUsuario}";
+        }
+        /// <summary>
+        /// Corrobora la pertenencia de un nombre de usuario y password en la base de datos
+        /// </summary>
+        /// <param name="nombreUsuario"></param>
+        /// <param name="password"></param>
+        /// <returns>Retorno true si hay coinsidencia</returns>
+        public static bool IdentificarUsuario(string nombreUsuario, string password)
+        {
+            bool retorno = false;
+            string path = null;
+            if(((path=GetFolderPath(SpecialFolder.Desktop)+@"\TP4\") is not null)) 
+            {
+                try
+                {
+                    Serializar<List<UsuarioDTV>> listaSerializada = new Serializar<List<UsuarioDTV>>();
+                    List<UsuarioDTV> listaUsuariosDTV = listaSerializada.Leer(path, "usuariosDTV.xml");
+                    foreach (UsuarioDTV item in listaUsuariosDTV)
+                    {
+                        if(item.NombreUsuario==nombreUsuario && item.Passwword==password)
+                        {
+                            retorno = true;
+                            return retorno;
+                        }
+                    }
+                    throw new AccesoNoAutorizadoException("Combinacio Usuario / Password incorreta");
+                }
+                catch (ArchivoException)
+                {
+                    throw;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return retorno;
+        }
+    }
+}
